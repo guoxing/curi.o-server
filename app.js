aa4be42eb81ae4e4634b0504d46c37bee47d34f6
Catherine Mullings <Cat@dn0a22b84d.sunet>
2014-05-12 13:41:17 -0700

    User response shown when click

--- app.js
+++ app.js
@@ -51,6 +51,7 @@ app.post('/api/visit-times', logBrowse.logVisitTimes);
 app.post('/api/answers', logBrowse.logAnswers);
 app.post('/api/estimate-active-time', logBrowse.logEstimateAT);
 app.post('/api/estimate-visit-times', logBrowse.logEstimateVT);
+app.get('/api/user-responses', logBrowse.getUserResponses);
 
 /// catch 404 and forwarding to error handler
 app.use(function(req, res, next) {
